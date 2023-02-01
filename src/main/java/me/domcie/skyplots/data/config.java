43975f02c8999d0470bb6a1df2e890ab180395bc
DomCie99 <dominikciesielski4@gmail.com>
2023-02-01 15:11:26 +0100

    ---

--- src/main/java/me/domcie/skyplots/data/config.java
+++ src/main/java/me/domcie/skyplots/data/config.java
@@ -93,60 +93,60 @@ public class config {
 
         this.playerGui = cfg.getStringList("messages.playerGui");
         this.adminGui = cfg.getStringList("messages.adminGui");
+        playerGui.replaceAll(s -> s.replaceAll("&", "§"));
+        adminGui.replaceAll(s -> s.replaceAll("&", "§"));
 
+        this.msg_player_only = cfg.getString("messages.msg_player_only").replace("&", "§");
+        this.msg_player_offline = cfg.getString("messages.msg_player_offline").replace("&", "§");
+        this.msg_player_null = cfg.getString("messages.msg_player_null").replace("&", "§");
+        this.msg_no_permission = cfg.getString("messages.msg_no_permission").replace("&", "§");
 
+        this.msg_has_pending_invitation = cfg.getString("messages.msg_has_pending_invitation").replace("&", "§");
 
-        this.msg_player_only = cfg.getString("messages.msg_player_only".replace("&", "§"));
-        this.msg_player_offline = cfg.getString("messages.msg_player_offline".replace("&", "§"));
-        this.msg_player_null = cfg.getString("messages.msg_player_null".replace("&", "§"));
-        this.msg_no_permission = cfg.getString("messages.msg_no_permission".replace("&", "§"));
+        this.msg_create = cfg.getString("messages.msg_create").replace("&", "§");
+        this.msg_created = cfg.getString("messages.msg_created").replace("&", "§");
+        this.msg_teleport = cfg.getString("messages.msg_teleport").replace("&", "§");
+        this.msg_usage = cfg.getString("messages.msg_usage").replace("&", "§");
 
-        this.msg_has_pending_invitation = cfg.getString("messages.msg_has_pending_invitation".replace("&", "§"));
+        this.msg_not_owner = cfg.getString("messages.msg_not_owner").replace("&", "§");
+        this.msg_player_not_owner = cfg.getString("messages.msg_player_not_owner").replace("&", "§");
 
-        this.msg_create = cfg.getString("messages.msg_create".replace("&", "§"));
-        this.msg_created = cfg.getString("messages.msg_created".replace("&", "§"));
-        this.msg_teleport = cfg.getString("messages.msg_teleport".replace("&", "§"));
-        this.msg_usage = cfg.getString("messages.msg_usage".replace("&", "§"));
+        this.msg_not_member = cfg.getString("messages.msg_not_member").replace("&", "§");
+        this.msg_player_not_member = cfg.getString("messages.msg_player_not_member").replace("&", "§");
 
-        this.msg_not_owner = cfg.getString("messages.msg_not_owner".replace("&", "§"));
-        this.msg_player_not_owner = cfg.getString("messages.msg_player_not_owner".replace("&", "§"));
+        this.msg_no_island = cfg.getString("messages.msg_no_island").replace("&", "§");
+        this.msg_has_island = cfg.getString("messages.msg_has_island").replace("&", "§");
 
-        this.msg_not_member = cfg.getString("messages.msg_not_member".replace("&", "§"));
-        this.msg_player_not_member = cfg.getString("messages.msg_player_not_member".replace("&", "§"));
+        this.msg_no_location = cfg.getString("messages.msg_no_location").replace("&", "§");
 
-        this.msg_no_island = cfg.getString("messages.msg_no_island".replace("&", "§"));
-        this.msg_has_island = cfg.getString("messages.msg_has_island".replace("&", "§"));
+        this.msg_no_schematic = cfg.getString("messages.msg_no_schematic").replace("&", "§");
+        this.msg_empty_clipboard = cfg.getString("messages.msg_empty_clipboard").replace("&", "§");
+        this.msg_error_saving = cfg.getString("messages.msg_error_saving").replace("&", "§");
+        this.msg_error_pasting = cfg.getString("messages.msg_error_pasting").replace("&", "§");
+        this.msg_success = cfg.getString("messages.msg_success").replace("&", "§");
 
-        this.msg_no_location = cfg.getString("messages.msg_no_location".replace("&", "§"));
+        this.msg_delete = cfg.getString("messages.msg_delete").replace("&", "§");
+        this.msg_delete_sender = cfg.getString("messages.msg_delete_sender").replace("&", "§");
+        this.msg_delete_receiver = cfg.getString("messages.msg_delete_receiver").replace("&", "§");
 
-        this.msg_no_schematic = cfg.getString("messages.msg_no_schematic".replace("&", "§"));
-        this.msg_empty_clipboard = cfg.getString("messages.msg_empty_clipboard".replace("&", "§"));
-        this.msg_error_saving = cfg.getString("messages.msg_error_saving".replace("&", "§"));
-        this.msg_error_pasting = cfg.getString("messages.msg_error_pasting".replace("&", "§"));
-        this.msg_success = cfg.getString("messages.msg_success".replace("&", "§"));
+        this.msg_remove = cfg.getString("messages.msg_remove").replace("&", "§");
+        this.msg_remove_sender = cfg.getString("messages.msg_remove_sender").replace("&", "§");
+        this.msg_remove_receiver = cfg.getString("messages.msg_remove_receiver").replace("&", "§");
 
-        this.msg_delete = cfg.getString("messages.msg_delete".replace("&", "§"));
-        this.msg_delete_sender = cfg.getString("messages.msg_delete_sender".replace("&", "§"));
-        this.msg_delete_receiver = cfg.getString("messages.msg_delete_receiver".replace("&", "§"));
+        this.msg_confirm = cfg.getString("messages.msg_confirm").replace("&", "§");
+        this.msg_confirm_send = cfg.getString("messages.msg_confirm_send").replace("&", "§");
+        this.msg_confirm_expired = cfg.getString("messages.msg_confirm_expired").replace("&", "§");
 
-        this.msg_remove = cfg.getString("messages.msg_remove".replace("&", "§"));
-        this.msg_remove_sender = cfg.getString("messages.msg_remove_sender".replace("&", "§"));
-        this.msg_remove_receiver = cfg.getString("messages.msg_remove_receiver".replace("&", "§"));
+        this.msg_invitation_send = cfg.getString("messages.msg_invitation_send").replace("&", "§");
+        this.msg_invitation_receive = cfg.getString("messages.msg_invitation_receive").replace("&", "§");
+        this.msg_invitation_expired = cfg.getString("messages.msg_invitation_expired").replace("&", "§");
+        this.msg_no_invitation = cfg.getString("messages.msg_no_invitation").replace("&", "§");
 
-        this.msg_confirm = cfg.getString("messages.msg_confirm".replace("&", "§"));
-        this.msg_confirm_send = cfg.getString("messages.msg_confirm_send".replace("&", "§"));
-        this.msg_confirm_expired = cfg.getString("messages.msg_confirm_expired".replace("&", "§"));
+        this.msg_invitation_accept = cfg.getString("messages.msg_invitation_accept").replace("&", "§");
+        this.msg_invitation_accepted = cfg.getString("messages.msg_invitation_accepted").replace("&", "§");
 
-        this.msg_invitation_send = cfg.getString("messages.msg_invitation_send".replace("&", "§"));
-        this.msg_invitation_receive = cfg.getString("messages.msg_invitation_receive".replace("&", "§"));
-        this.msg_invitation_expired = cfg.getString("messages.msg_invitation_expired".replace("&", "§"));
-        this.msg_no_invitation = cfg.getString("messages.msg_no_invitation".replace("&", "§"));
-
-        this.msg_invitation_accept = cfg.getString("messages.msg_invitation_accept".replace("&", "§"));
-        this.msg_invitation_accepted = cfg.getString("messages.msg_invitation_accepted".replace("&", "§"));
-
-        this.msg_invitation_decline = cfg.getString("messages.msg_invitation_decline".replace("&", "§"));
-        this.msg_invitation_declined = cfg.getString("messages.msg_invitation_declined".replace("&", "§"));
+        this.msg_invitation_decline = cfg.getString("messages.msg_invitation_decline").replace("&", "§");
+        this.msg_invitation_declined = cfg.getString("messages.msg_invitation_declined").replace("&", "§");
     }
 
     public static config getInst(){
